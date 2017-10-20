64669d0145d38b6e66903216ac5be50fc22fe9a6
lawpachi <15802994779@163.com>
2017-10-20 10:13:34 +0800

    重新开始

--- index.js
+++ index.js
@@ -1,29 +1,14 @@
 var path = require('path');
 var express = require('express');
-
-var config = require('config-lite')(__dirname);
-var routes = require('./routes');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+var flash = require('connect-flash');
+var winston = require('winston');
+var expressWinston = require('express-winston');
 
 
 var app = express();
 
-// 设置模板目录
-app.set('views', path.join(__dirname, 'views'));
-// 设置模板引擎为 ejs
-app.set('view engine', 'ejs');
-
-// 设置静态文件目录
-app.use(express.static(path.join(__dirname, 'public')));
-
-// 路由
-routes(app);
-
-
-if (module.parent) {
-  module.exports = app;
-} else {
-  // 监听端口，启动程序
-  app.listen(config.port, function () {
-    console.log(`istening on port ${config.port}`);
-  });
-}
+app.listen(3030, function() {
+    console.log('开始监听')
+})
--- routes/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (app) {
-    app.get('/', function (req, res) {
-        res.redirect('/signin');
-    });
-    app.use('/signin', require('./signin'));
-    app.use('/signup', require('./signup'));
-}
