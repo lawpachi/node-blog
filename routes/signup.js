8f2b73cf28d41318188a13257dd5bf7780915c11
lawpachi <15802994779@163.com>
2017-10-20 11:53:22 +0800

    使用session

--- routes/signup.js
+++ routes/signup.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+var fs = require('fs');
 var router = express.Router();
 var sha1 = require('sha1');
 var UserModel = require('../models/users');
@@ -7,7 +8,7 @@ var UserModel = require('../models/users');
 router.get('/', function (req, res) {
     res.render('signup')
 });
-router.post('/', function (req, res) {
+router.post('/', function (req, res, next) {
     var name = req.fields.name;
     var gender = req.fields.gender;
     var bio = req.fields.bio;
@@ -24,7 +25,22 @@ router.post('/', function (req, res) {
         bio: bio,
         avatar: avatar
     };
-     UserModel.create(user);
+    UserModel.create(user).then(
+        function (result) {
+            // 此 user 是插入 mongodb 后的值，包含 _id
+            user = result.ops[0];
+            // 将用户信息存入 session
+            delete user.password;
+            req.session.user = user;
+        }
+    ).catch(function (e) {
+        // 注册失败，异步删除上传的头像
+        fs.unlink(req.files.avatar.path);
+        if (e.message.match('E11000 duplicate key')) {
+            return res.redirect('/signup');
+        }
+        next(e);
+    })
     // res.render('signup')
 });
 module.exports = router;
